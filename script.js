2097dbec58a41ea5d08f5aa042a8672d95997da6
izsi-salmon <izsi.salmon@gmail.com>
2019-03-18 11:49:02 +1300

    improved stick nav, document title change on blur, thin nav bar, nav bar typography changes

--- script.js
+++ script.js
@@ -9,23 +9,33 @@ var dropdownContent = document.getElementById('dropdownMenu');
 // Sticky nav bar variables
 var navbar = document.getElementById('navbar');
 var freeQuoteCta = document.getElementById('freequote');
+var pageContent = document.getElementById('swiperContainer');
 var sticky = navbar.offsetHeight;
+// Document title DOM queries
+var docTitle = 'CMOS – Commercial Cleaning Company';
+var titleMessage = 'Hey, come back!';
 
+// Function controlling sticky nav ability
 function stickyNav() {
     var navHeight = navbar.scrollHeight;
+    var ctaHeight = freeQuoteCta.scrollHeight;
     if (window.pageYOffset >= sticky) {
     navbar.classList.add('sticky-header');
     freeQuoteCta.classList.add('sticky-mobile-cta');
     freeQuoteCta.style.top = navHeight + 'px';
     mobileNavContent.style.position = 'fixed';
     mobileNavContent.style.top = navHeight + 'px';
+    pageContent.style.marginTop = navHeight + ctaHeight +'px';
+    console.log(pageContent.style.marginTop);
   } else {
     navbar.classList.remove('sticky-header');
     freeQuoteCta.classList.remove('sticky-mobile-cta');
+    pageContent.style.marginTop = 0;
   }
 }
 window.onscroll = function() {stickyNav()};
 
+// Function controlling hamburger menu (on mobile/tablet only)
 function toggleMenu(){
     if(mobileNavContent.style.maxHeight){
         mobileNavContent.style.maxHeight = null;
@@ -42,6 +52,7 @@ hamburgerButton.addEventListener('click', toggleMenu, false);
 
 dropshadow.addEventListener('click', toggleMenu, false);
 
+// Function controlling drop-down services menu (desktop only)
 function toggleDropdown(){
     if(dropdownContent.style.maxHeight){
         dropdownContent.style.maxHeight = null;
@@ -57,6 +68,18 @@ dropdownButton.addEventListener("click", function(event){
 dropdownButton.addEventListener('click', toggleDropdown, false);
 
 dropdownArrow.addEventListener('click', toggleDropdown, false);
+
+
+// Function controlling document title change
+
+window.onblur = function() { 
+    document.title = titleMessage; 
+}
+
+window.onfocus = function() {
+    document.title = docTitle; 
+}
+
 
 // SWIPER (Home page slideshow banner)
 
